f53755e03f69a1e40e9cd4f86539d0cdc2167893
Nanthitha-sri <153504089+Nanthitha-sri@users.noreply.github.com>
2024-04-13 21:35:13 -0700

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ import streamlit as st
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 
-CLIENT_ID = "70a9fb89662f4dac8d07321b259eaad7"
-CLIENT_SECRET = "4d6710460d764fbbb8d8753dc094d131"
+CLIENT_ID = #client_id
+CLIENT_SECRET = #client_secret
 
 # Initialize the Spotify client
 client_credentials_manager = SpotifyClientCredentials(client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
